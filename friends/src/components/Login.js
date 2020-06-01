588ff1d2e3776784145c4369ea915a40beb2aa13
cknoettg <cknoettg@yahoo.com>
2020-05-31 21:55:07 -0400

    Added Spinner

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PacmanLoader from 'react-spinners';
 
 //Login Form
 //  ICE
@@ -9,23 +10,33 @@ export const Login = props => {
     //can we combine this into one state statement?
     const [uname, setUname] = useState('');
     const [password, setPassword] = useState('');
+    //isLoading - prop for spinner is called 'loading'
+
 
     //change handler
     const handleChanges = e => {
 
     }
 
-    //authenticator
+    //authenticator - login function
     const login = e => {
-
+        e.preventDefault();
+        //use localStorage
     }
 
     //render
+    //  reminder: create Spinner
+    //  React docs on Spinners: https://www.npmjs.com/package/react-spinners
     return (
         <form onSubmit={login}>
             <input type="text" value={uname} name={uname} onChange={handleChanges} placeholder="Username" />
             <input type="password" value={password} name={password} onChange={handleChanges} placeholder="Password" />
             <button onClick={onSubmit}>Login</button>
+            <PacmanLoader
+            loading="true"
+            color="#000000"
+            css="">
+            </PacmanLoader>
         </form>
     )
 }
