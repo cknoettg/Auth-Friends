e6185b75f1a852748566d1bebafdd3f96b6fc531
cknoettg <cknoettg@yahoo.com>
2020-05-31 22:29:54 -0400

    Added private and protected routes

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -4,6 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
+    // baseURL: "http://localhost:5000",
     headers: {
       Authorization: token
     }
