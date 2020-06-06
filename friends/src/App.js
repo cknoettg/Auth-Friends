076fb7184025e0332f002c1802456ed481d475ae
cknoettg <cknoettg@yahoo.com>
2020-06-05 21:52:57 -0400

    Finished MVP. Needs polishing.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import Login from "./components/Login";
 
-//Add Route For Login Page
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import PrivateRoute from "./utils/PrivateRoute";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Auth Friends App</h1>
-      <Router>
-        <Route exact path="/" component={Login} />
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+         <Link to="/login"> Login</Link>
+        </div>
+        <div>
+          <Link to="/protected">Friends List</Link>
+        </div>
+       
+
+        <Switch>
+          <PrivateRoute path="/protected" component={FriendsList}/>
+          <Route path="/login" component={Login}/>
+          <Route component={Login}/>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+//EARLY ATTEMPT:
+// import React from 'react';
+// import './App.css';
+// import { Route, Router } from 'react-router-dom';
+// import Login from "./components/Login";
+
+// //Add Route For Login Page
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Auth Friends App</h1>
+//       <Router>
+//         <Route exact path="/" component={Login} />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,61 +1,124 @@
-import React, { useState } from 'react';
-//import axios from 'axios';
-import { axiosWithAuth } from "../utils/axiosWithAuth";
-import PacmanLoader from 'react-spinners';
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-//Problem with react-spinners - requires react16 up
+import Loader from 'react-loader-spinner';
 
-//Login Form
-//  ICE
-//  Form with state, handle changes, authenticator
-
-const Login = props => {
-    //we want name, password, isLoading
-    const [login, setLogin] = useState({
-        username: '',
-        password: '',
-        isLoading: false
-    });
-
-    //change handler
-    const handleChanges = e => {
-        setLogin({ ...login, [e.target.name]: e.target.value });
-    }
-
-    //authenticator - login function
-    const logins = e => {
-        e.preventDefault();
-        //use localStorage
-        axiosWithAuth()
-            .post("http://localhost:5000/api/login", login)
-            .then(res => {
-                localStorage.setItem("token", res.data.payload);
-                props.history.push('/friends');
-                console.log("Successful login", res.data);
-            })
-            .catch(err => console.log(err.response));
-        setLogin({
+class Login extends React.Component{
+    state = {
+        credentials: {
             username: '',
             password: '',
-            isLoading: false
-        })
+        },
+        isFetching: false
     }
 
-    //render
-    //  reminder: create Spinner
-    //  React docs on Spinners: https://www.npmjs.com/package/react-spinners
-    return (
-        <form onSubmit={logins}>
-            <input type="text" value={props.uname} name={props.uname} onChange={handleChanges} placeholder="Username" />
-            <input type="password" value={props.password} name={props.password} onChange={handleChanges} placeholder="Password" />
-            <button onClick={props.onSubmit}>Login</button>
-            <PacmanLoader
-            loading="true"
-            color="#000000"
-            css="">
-            </PacmanLoader>
-        </form>
-    )
-}
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
 
+    login = e => {
+        e.preventDefault();
+        this.setState({
+            isFetching: true
+        });
+
+        axiosWithAuth()
+        .post('/login', this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            this.props.history.push('/protected');
+        })
+        .catch(err => console.log(err));
+    };
+
+
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.login}>
+                    <input type="text" name="username" value={this.state.credentials.username} onChange={this.handleChange}/>
+
+                    <input type="password" name="password" value={this.state.credentials.password} onChange={this.handleChange}/>
+
+                    <button>Login</button>
+                    {this.state.isFetching && (
+                    <div className="key spinner">
+                        <Loader type="Puff" color="#204963" height="60" width="60" />
+                        <p>Loading Data</p>
+                    </div>
+                     )}
+                </form>
+            </div>
+        )
+    }
+}
 export default Login;
+
+//EARLY Attempt:
+// import React, { useState } from 'react';
+// //import axios from 'axios';
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+// import PacmanLoader from 'react-spinners';
+
+// //Problem with react-spinners - requires react16 up
+
+// //Login Form
+// //  ICE
+// //  Form with state, handle changes, authenticator
+
+// const Login = props => {
+//     //we want name, password, isLoading
+//     const [login, setLogin] = useState({
+//         username: '',
+//         password: '',
+//         isLoading: false
+//     });
+
+//     //change handler
+//     const handleChanges = e => {
+//         setLogin({ ...login, [e.target.name]: e.target.value });
+//     }
+
+//     //authenticator - login function
+//     const logins = e => {
+//         e.preventDefault();
+//         //use localStorage
+//         axiosWithAuth()
+//             .post("http://localhost:5000/api/login", login)
+//             .then(res => {
+//                 localStorage.setItem("token", res.data.payload);
+//                 props.history.push('/friends');
+//                 console.log("Successful login", res.data);
+//             })
+//             .catch(err => console.log(err.response));
+//         setLogin({
+//             username: '',
+//             password: '',
+//             isLoading: false
+//         })
+//     }
+
+//     //render
+//     //  reminder: create Spinner
+//     //  React docs on Spinners: https://www.npmjs.com/package/react-spinners
+//     return (
+//         <form onSubmit={logins}>
+//             <input type="text" value={props.uname} name={props.uname} onChange={handleChanges} placeholder="Username" />
+//             <input type="password" value={props.password} name={props.password} onChange={handleChanges} placeholder="Password" />
+//             <button onClick={props.onSubmit}>Login</button>
+//             <PacmanLoader
+//             loading="true"
+//             color="#000000"
+//             css="">
+//             </PacmanLoader>
+//         </form>
+//     )
+// }
+
+// export default Login;
